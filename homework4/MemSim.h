0e5879aa4dbe9d18bc25cbdfbe5ffc34cb4ed38d
gildnej <gildnej@wwu.edu>
2019-05-28 15:53:57 -0700

    initialization with working make file to generate the executeable

---
+++ homework4/MemSim.h
@@ -0,0 +1,10 @@
+/*
+ *	MMUsim.h
+ *
+ *	Header file for simulator for CSCI 447 memory management exercise
+ *
+ *	Filip Jagodzinski, Computer Science, WWU, 21 February 2018
+ */
+
+void Simulate(int rounds);
+

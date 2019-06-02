66855daa3877fec56dddd41f565cbcd2676da4a6
gildnej <gildnej@wwu.edu>
2019-06-01 17:54:55 -0700

    Just about finished, working as expected

--- homework4/PageSim.c
+++ homework4/PageSim.c
@@ -8,108 +8,360 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "MemSim.h"
+#include <string.h>
+#include <stdbool.h>
+#include <time.h>
+#include <limits.h>
 
-#define TABLESIZE	97		// size of hash table of processes
+#include "MemSim.h"
+#include "PageSim.h"
+
+#define TABLESIZE	2048		// size of hash table of processes
 #define MAXPROC		64		// maximum number of processes in the system
 #define PAGESIZE	4096		// system page size
+#define LFUHALFREQ 5000
+
+enum{
+  METHOD_LRU = 1,
+  METHOD_LFU,
+  METHOD_RAND,
+};
 
 // data structure for process hash table entries
-typedef struct table_struct {
-  int pid;
-  int page;
-  struct procstruct* next;
-  bool dirty;
-  bool ref;
-} ptable;
 
-ptable* pagetable[TABLESIZE];		// process hash table
-int procCount = 0;		// number of processes
-int pageCount = 0;
 
-// look for pid in the hash table
-// if pid found, return 1
-// if pid not found and number of processes < MAXPROC, add pid to hash table and return 1
-// if pid not found and number of processes >= MAXPROC, return 0
+ptable* pagetable[TABLESIZE];   // process hash table
+int procs[MAXPROC];
+int procCount = 0;    // number of processes
+int lastReplace;
+int method;
 
-int find(int pid, int page) {
-  int index = (pid + page) % TABLESIZE;
-  proc* node = table[index];
-  proc* prev = NULL;
-  
-  // look along the chain for this hash table index
-  while (node != NULL && (node->pid != pid || node ->page != page)) {
-    prev = node;
-    node = node->next;
-  }
-  
-  // pid not found in hash table
-  if (node == NULL) {
-    if (procCount >= MAXPROC)
-      return 0;		// too many processes
-    else {				// add new process
-      node = (proc*) malloc(sizeof(proc));j
-      node->page = page;
-      node->next = NULL;
-      procCount++;
-      pageCount++;
-      if (prev == NULL)
-	table[index] = node;
-      else
-	prev->next = node;
-    }
-  }
-  return 1;
-}
+int writeBackFaults = 0;
+int readFaults = 0;
+int pageAttempts = 0;
 
-// remove a pid from the hash table
-void Remove(int pid) {
-  int index = pid % TABLESIZE;
-  proc* node = table[index];
-  proc* prev = NULL;
-  
-  // look along the chain for this hash table index
-  while (node != NULL && node->pid != pid) {
-    prev = node;
-    node = node->next;
+int main(int argc, char* argv[]) {
+  srand(time(0)); 
+
+  if(argc < 2){
+    printf("Please specify a valid replacement method (LRU, LFU, Random");
+    exit(1);
   }
-  
-  // if pid found, remove it
-  if (node != NULL) {
-    if (prev == NULL)
-      table[index] = node->next;
-    else
-      prev->next = node->next;
-    free(node);
+  else if (!strcmp(argv[1], "LRU")){
+    method = METHOD_LRU;
   }
+  else if (!strcmp(argv[1], "LFU")){
+    method = METHOD_LFU;
+  }
+  else if (!strcmp(argv[1], "Random")){
+    method = METHOD_RAND;
+  } else{
+    printf("Please specify a valid replacement method (LRU, LFU, Random");
+    exit(1);
+  }
+
+  for(int i=0; i<TABLESIZE; i++){
+    pagetable[i] = NULL;
+  }
+
+  for(int i=0; i<MAXPROC; i++){
+    procs[i] = -1;
+  }
+
+  printf("MMU simulation with %s replacement strategy started\n", argv[1]);
+  Simulate(TABLESIZE*1000);
+
+  printf("\n");
+  printf("MMU simulation with %s replacement strategy finished\n", argv[1]);
+
+  printf("\n");
+  printf("Total Page Attempts: %d\n", pageAttempts);
+  printf("Write Back Faults: %d\n", writeBackFaults);
+  printf("Read Faults: %d\n", readFaults);
+  printf("Total Faults: %d\n\n", writeBackFaults+readFaults);
+
+  double faultFreq = 100*(double)(writeBackFaults+readFaults)/(double)(pageAttempts);
+  double wFaultFreq = 100*(double)writeBackFaults/(double)(pageAttempts);
+
+  printf("Fault Freq: %.2f%%\n",faultFreq);
+  printf("Write Back Fault Freq: %.2f%%\n",wFaultFreq);
 }
 
 // called in response to a memory access attempt by process pid to memory address
 int Access(int pid, int address, int write) {
+  pageAttempts++;
 
-  if (find(pid, address/PAGESIZE)) {
-    printf("pid %d wants %s access to address %d on page %d. Page count is %d\n", 
-	   pid, (write) ? "write" : "read", address, address/PAGESIZE, pageCount);
-    return 1;
-  } else {
-    printf("pid %d refused\n", pid);
-    return 0;
+  if(pageAttempts%LFUHALFREQ == 0) halfReferences();
+
+  int page = address/PAGESIZE;
+  int retval = 0;
+
+  if(validProcess(pid)){
+    if(available(pid, page, write));
+    else if(replaceEmpty(pid, page, write));
+    else if(method == METHOD_LRU){
+      replaceLRU(pid, page, write);
+    }
+    else if(method == METHOD_LFU){
+      replaceLFU(pid, page, write);
+    }
+    else if(method == METHOD_RAND){
+      replaceRand(pid, page, write);
+    }
+    else{
+      printf("No method specified \n");
+      exit(1);
+    }
+    retval = 1;
+
+    //printf("pid %d wants %s access to address %d on page %d \n",pid, (write) ? "write" : "read", address, address/PAGESIZE);
   }
+  else if(procCount < MAXPROC){
+    fillProcSlot(pid);
+    pageAttempts--;
+    Access(pid, address, write);
+  }
+  else{
+    //printf("pid %d refused, max processes reached \n", pid);
+    retval = 0;
+  }
+
+  return retval;
+}
+
+bool available(int pid, int page, int write){
+  bool success = false;
+  for(int i=0; i<TABLESIZE; i++){
+
+    if(pagetable[i] != NULL && pagetable[i]->pid == pid && pagetable[i]->page == page){
+      pagetable[i]->dirty = write;
+      pagetable[i]->ref = 1;
+      pagetable[i]->refCount++;
+      success = true;
+      break;
+    }
+
+  }
+  return success;
+}
+
+bool replaceEmpty(int pid, int page, int write){
+  bool success = false;
+  for(int i=0; i<TABLESIZE; i++){
+
+    if(pagetable[i] == NULL){
+      pagetable[i] = newTableEntry(pid, page, write);
+      readFaults++;
+      success = true;
+      lastReplace = i;
+      break;
+    }
+
+  }
+  return success;
+
+}
+
+void replaceLRU(int pid, int page, int write){
+  bool found = false;
+
+  if(!found){
+    for(int i=0; i<TABLESIZE; i++){
+      int choice = (i+lastReplace) % TABLESIZE;
+      ptable* p = pagetable[choice];
+
+      if(p != NULL && p->dirty == 0 && p->ref ==0){
+        found = true;
+        pagetable[choice] = newTableEntry(pid, page, write);
+        readFaults++;
+        lastReplace = choice;
+        break;
+      }else if(p != NULL){
+        p->ref = 0;
+      }
+
+    }
+  }
+
+  if(!found){
+    for(int i=0; i<TABLESIZE; i++){
+      int choice = (i+lastReplace) % TABLESIZE;
+      ptable* p = pagetable[choice];
+
+      if(p != NULL && p->dirty == 1 && p->ref ==0){
+        found = true;
+        pagetable[choice] = newTableEntry(pid, page, write);
+        writeBackFaults++;
+        lastReplace = choice;
+        break;
+      }else if(p != NULL){
+        p->ref = 0;
+      }
+    }
+  }
+
+  if(!found){
+    for(int i=0; i<TABLESIZE; i++){
+      int choice = (i+lastReplace) % TABLESIZE;
+      ptable* p = pagetable[choice];
+
+      if(p != NULL && p->dirty == 0 && p->ref == 1){
+        found = true;
+        pagetable[choice] = newTableEntry(pid, page, write);
+        readFaults++;
+        lastReplace = choice;
+        break;
+      }else if(p != NULL){
+        p->ref = 0;
+      }
+
+    }
+  }
+
+  if(!found){
+    for(int i=0; i<TABLESIZE; i++){
+      int choice = (i+lastReplace) % TABLESIZE;
+      ptable* p = pagetable[choice];
+
+      if(p != NULL && p->dirty == 1 && p->ref ==1){
+        found = true;
+        pagetable[choice] = newTableEntry(pid, page, write);
+        writeBackFaults++;
+        lastReplace = choice;
+        break;
+      }else if(p != NULL){
+        p->ref = 0;
+      }
+
+    }
+  }
+
+}
+
+void replaceLFU(int pid, int page, int write){
+  int minRef = INT_MAX;
+  int minRefIndex =-1;
+
+  for(int i=0; i<TABLESIZE; i++){
+    ptable* p = pagetable[i];
+
+    if(p != NULL && p->ref < minRef){
+      minRef = p->ref;
+      minRefIndex = i;
+    }
+  }
+
+  if(minRefIndex == -1){
+    printf("You messed up\n");
+    exit(1);
+  }
+
+  ptable* evicted = pagetable[minRefIndex];
+
+  if(evicted->dirty){
+    writeBackFaults++;
+  }else{
+    readFaults++;
+  }
+
+  printf("%d\n",minRef);
+  pagetable[minRefIndex] = newTableEntry(pid, page, write);
+  lastReplace = minRefIndex;
+
+}
+
+void replaceRand(int pid, int page, int write){
+  int choice = (rand() % (TABLESIZE));
+  ptable* evicted = pagetable[choice];
+  pagetable[choice] = newTableEntry(pid,page,write);
+  lastReplace = choice;
+
+  if(evicted == NULL){
+    printf("That shouldn't have happened\n");
+    printf("Choice: %d\n",choice);
+    exit(1);
+  }
+
+  if(evicted->dirty){
+    writeBackFaults++;
+  }else{
+    readFaults++;
+  }
+
+  
 }
 
 // called when process terminates
 void Terminate(int pid) {
-  printf("pid %d terminated\n", pid);
-  procCount--;
-  Remove(pid);
+
+  for(int i=0; i<MAXPROC; i++){
+
+    if(procs[i] == pid){
+      //printf("pid %d terminated\n", pid);
+      procs[i] = -1;
+      procCount--;
+      break;
+    }
+
+    for(int i=0; i<TABLESIZE; i++){
+
+      if(pagetable[i] != NULL && pagetable[i]->pid == pid){
+        if(pagetable[i]->dirty) writeBackFaults++;
+        pagetable[i] = NULL;
+      }
+
+    }
+
+  }
+  
 }
 
-int main() {
-  int i;
-  // initialize the process hash table
-  for (i = 0; i < TABLESIZE; i++) table[i] = NULL;
-  
-  printf("MMU simulation started\n");
-  Simulate(200);
-  printf("MMU simulation completed\n");
+ptable* newTableEntry(int pid, int page, int write){
+  ptable* newEntry = malloc(sizeof(ptable));
+
+  if(newEntry == NULL){
+    perror("Malloc");
+    exit(1);
+  }
+
+  newEntry->pid = pid;
+  newEntry->page =page;
+  newEntry->dirty = write;
+  newEntry->ref = 1;
+  newEntry->refCount = 0;
+
+  return newEntry;
+}
+
+bool validProcess(int pid){
+  bool valid = false;
+
+  for(int i=0; i<MAXPROC; i++){
+    if(procs[i] == pid) valid = true;
+  }
+
+  return valid;
+
+}
+
+void fillProcSlot(int pid){
+
+  for(int i=0; i<MAXPROC; i++){
+
+    if(procs[i] == -1){
+      procs[i] = pid;
+      procCount++;
+      break;
+    }
+
+  }
+
+}
+
+void halfReferences(){
+  for(int i=0; i<TABLESIZE; i++){
+    if(pagetable[i] != NULL){
+      pagetable[i]->ref /= 2;
+    }
+  }
 }

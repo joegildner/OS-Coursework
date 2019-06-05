b94d60f22c25d95fa3222bc1a4660bcab4fc2b10
gildnej <gildnej@wwu.edu>
2019-06-05 10:34:32 -0700

    arg parsing up and switching correctly

--- homework5/DiskSim.java
+++ homework5/DiskSim.java
@@ -1,7 +1,18 @@
 public class DiskSim{
 
   public static void main(String[] args){
-    System.out.println("Jello");
+
+    switch (args.length){
+      case 1:
+        if(args[0].equals("R")) System.out.println("Random Mode");
+        break;
+      case 3:
+        if(args[2].charAt(0) == 'R') System.out.println("Random requests");
+        break;
+      default:
+        System.out.println("Selected requests");
+    }
+
   }
 
 
